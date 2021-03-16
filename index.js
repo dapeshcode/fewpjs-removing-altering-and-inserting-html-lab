748cae42d4b486b214d6ecf371f9900c963061b0
dapeshcode <ampeccia@gmail.com>
2021-03-16 18:17:50 -0500

    commit tree 2ee2be9080e423e4c3dfb664976af332302c8280

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let main = document.querySelector('main#main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = "Alex is the champion"
